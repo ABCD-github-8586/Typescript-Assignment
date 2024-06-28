469bb65f2d6765b5547cab9c5c948460612848c4
ABCD-github-8586 <faizanarain8586@gmail.com>
2024-06-28 20:40:37 +0500

    Add files via upload

---
+++ 12-Greeting Message.ts
@@ -0,0 +1,10 @@
+let names : string [] = ['Daniyal','Abdul Rehman','Faizan','Ihsan','Haji Aslam'];
+
+const message : string = '  :Most welcome to you in your first typescript class! '
+
+for( let i=0; i<names.length; i++){
+
+
+    console.log(names[i]+message)
+
+}
