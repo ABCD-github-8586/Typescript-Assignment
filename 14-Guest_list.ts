022533865d5f6dd7a05ed0807ba1419c4cbc5cd9
ABCD-github-8586 <faizanarain8586@gmail.com>
2024-06-28 20:42:28 +0500

    Add files via upload

---
+++ 14-Guest_list.ts
@@ -0,0 +1,7 @@
+let guest_list : string [] = ['Shahzaib','Amir','Hassan'];
+
+for(let i=0; i<guest_list.length; i++){
+
+
+console.log('Dear Mr.' +guest_list[i] + ' \n\nIt is our honour to invite you in our annual distribution ceremony 2024.\n\nThanls a million!\n\n')
+}
