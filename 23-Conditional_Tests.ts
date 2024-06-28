aa2c91223eac857b30c6e89ac03ddb3ae7a76d76
ABCD-github-8586 <faizanarain8586@gmail.com>
2024-06-28 20:54:09 +0500

    Add files via upload

---
+++ 23-Conditional_Tests.ts
@@ -0,0 +1,45 @@
+// Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test.
+//  Your code should look something like this
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict true.")
+console.log(car == 'subaru')
+
+// Test no 01:-
+console.log("Is car === 'subaru'? I predict true.");
+console.log(car === 'subaru')
+
+// Test no 02:-
+console.log("Is car !== 'Toyota'? I predict true.");
+console.log(car !== 'Toyota');
+
+// Test no 03:-
+console.log("Is car !== 'Honda'? I predict true.");
+console.log(car !== 'Honda');
+
+// Test no 04:-
+console.log("Is car !== 'Ford'? I predict true.");
+console.log(car !== 'Ford');
+
+// Test no 05:-
+console.log("Is car.uppercase == 'SUBARU'? I predict true.");
+console.log(car.toUpperCase() == 'SUBARU');
+
+// Test no 06:-
+console.log("Is car == 'SUBARU'? I predict false.");
+console.log(car == 'SUBARU');
+
+// Test no 07:-
+console.log("Is car === 'SUBARU'? I predict false.");
+console.log(car === 'SUBARU');
+
+// Test no 08:-
+console.log("Is car === 'Train'? I predict false.");
+console.log(car === 'Train');
+
+// Test no 09:-
+console.log("Is car === 'Bus'? I predict false.");
+console.log(car === 'Bus');
+
+// Test no 10:-
+console.log("Is car === 'Cycle'? I predict false.");
+console.log(car === 'Cycle');
