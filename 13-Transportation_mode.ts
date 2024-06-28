5cc94b8e26a5a3513690378fe5d2edc0a8aa147a
ABCD-github-8586 <faizanarain8586@gmail.com>
2024-06-28 20:41:47 +0500

    Add files via upload

---
+++ 13-Transportation_mode.ts
@@ -0,0 +1,6 @@
+let transportation : string [] = ['cycle','motorbike','car','aeroplane','truck']
+
+for( let i=0; i<transportation.length; i++){
+
+    console.log('I would like to purchase a ' + transportation [i])
+}
